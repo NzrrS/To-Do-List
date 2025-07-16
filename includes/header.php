ce2d74be1b1a145cee9a59b2b3fa7ceaa70fcfc3
Nzrr <god.nizaryt10@gmail.com>
2025-07-16 07:36:11 +0100

    New structure

---
+++ includes/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <header>
+        
+    </header>
+    <script src="../Assets/script.js"></script>
+</body>
+</html>
