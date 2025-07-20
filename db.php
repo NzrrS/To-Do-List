a5a481ffeed9a989a02a508f5b41da90ff88d9c3
Nzrr <god.nizaryt10@gmail.com>
2025-07-20 18:31:28 +0100

    Finished and host it

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 try {
 
-    $dsn = 'mysql:host=sql104.infinityfree.com;dbname=if0_39518287_mytodo';
-    $user = 'if0_39518287';
-    $psw = 'xWy61raWM6';
+    $dsn = 'mysql:host=sql201.infinityfree.com;dbname=if0_39518719_todoo';
+    $user = 'if0_39518719';
+    $psw = 'nAeCqc8baYW';
     $pdo = new PDO($dsn, $user, $psw);
 } catch (PDOException $e) {
     die('erreur de connection' . $e->getMessage());
