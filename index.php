a854a89c413fb4f6295e1b135115df90efb4a2f7
Nzrr <164057221+Nzrr88@users.noreply.github.com>
2025-07-21 04:18:18 +0100

    Update index.php

--- index.php
+++ index.php
@@ -34,9 +34,6 @@ if ($id) {
 </head>
 
 <body>
-<!-- Sidebar -->
-
-
   <h1>TOdo App</h1>
   <h2>Welcome back, <?php echo htmlspecialchars($userName); ?> !</h2>
   <div class="container">
@@ -56,11 +53,8 @@ if ($id) {
       <a href="logout.php"> <svg fill="var(--secondary-color)" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px">
           <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z" />
         </svg></a>
-
     </button>
   </footer>
-
-
 </body>
 
 </html>
